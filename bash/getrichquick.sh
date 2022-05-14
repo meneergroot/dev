4d6a346e1001ec4c06ddf119a81d7be4d7c306e3
Meneer Groot <meneergroot@meneers-air.home>
2022-05-14 17:07:37 +0200

    getrichquick.sh

---
+++ bash/getrichquick.sh
@@ -0,0 +1,39 @@
+!#/bin/bash
+
+echo "What is your name?"
+
+read name
+
+echo "How old are you?"
+
+read age
+
+echo "Hello, $name, you are $age old."
+
+sleep 5
+
+echo "Do you wanna know your net-worth?"
+
+sleep 3
+
+echo "Yes?"
+
+sleep 3
+
+echo "Ok, let me do a quick search on the interwebz"
+
+sleep 2
+
+echo "."
+echo ".."
+echo "..."
+echo "...."
+echo "....."
+
+sleep 5
+
+echo "you are worth $RANDOM"
+
+
+
+
